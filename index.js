cd424d29e0e50575ba88f62ad6ea15aa7bd7407d
AsheToll <github email address>
2019-08-23 23:23:04 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,16 @@
+function Scooter(year, color, model) {
+    year,
+    color,
+    model
+}
+
+function Driver(name, age, experience) {
+    name,
+    age,
+    experience
+}
+
+function PickupLocation(address, city) {
+    address,
+    city
+}
